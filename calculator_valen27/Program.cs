20b15385170bd7955246f040889581b67dbfe20e
valentino Al Falaq <68651640+Valentino-Al@users.noreply.github.com>
2021-04-26 13:24:42 +0700

    initial commit

---
+++ calculator_valen27/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+ namespace calculator
+ {
+    class calculator
+    {
+            static void Main(string[] args)
+            {
+                int firstNum;
+                int secondNum;                   
+                string operation;
+                int answer;
+
+                Console.WriteLine("Hello Manieeez!, Selamat datang di kalkulator Valentino yey!");
+                Console.ReadLine();
+
+                Console.Write("Masukkan angka pertama ya maniezz: ");
+                firstNum = Convert.ToInt32(Console.ReadLine());                             
+
+                //User input for equation
+                Console.Write("Sekarang masukkan angka kedua: ");
+                secondNum = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Yuk mau pilih yang mana ( x , / , +, -) ");
+                operation = Console.ReadLine();
+
+                if (operation == "x")
+                {
+                   answer = firstNum * secondNum;
+                   Console.WriteLine(firstNum + " x " + secondNum + " = " + answer);
+                   Console.ReadLine();
+                }
+                else if (operation == "/")
+                {
+                   answer = firstNum / secondNum;
+                   Console.WriteLine(firstNum + " / " + secondNum + " = " + answer);
+                   Console.ReadLine();
+                }                                                                           
+                //Getting answers
+                else if (operation == "+")
+                {
+                   answer = firstNum + secondNum;
+                   Console.WriteLine(firstNum + " + " + secondNum + " = " + answer);
+                   Console.ReadLine();
+                }
+                else if (operation == "-")
+                {
+                   answer = firstNum - secondNum;
+                   Console.WriteLine(firstNum + " - " + secondNum + " = " + answer);
+                   Console.ReadLine();
+                }
+                else
+                {
+                   Console.WriteLine("Sorry that is not correct format! Please restart!");     //Catch
+                   Console.ReadLine();
+                }
+           }
+     }
+}
